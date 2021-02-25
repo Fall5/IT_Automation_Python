a968301949c70cbc5b5390cd36df609337256912
Hafifsyah Rifaldi <hafifsyahr@gmail.com>
2021-02-25 18:37:38 +0700

    Changing list code

--- CurrentUsers.py
+++ CurrentUsers.py
@@ -1,3 +1,10 @@
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
+
 def get_event_date(event):
     return event.date
 
@@ -19,22 +26,16 @@ def generate_report(machines):
             user_list = ", ".join(users)
             print("{}: {}".format(machine, user_list))
 
-class Event:
-    def __init__(self, event_date, event_type, machine_name, user):
-        self.date = event_date
-        self.type = event_type
-        self.machine = machine_name
-        self.user = user
+events = [
+    Event ('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event ('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event ('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+    Event ('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+    Event ('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+    Event ('2020-01-23 11:24:35', 'login', 'mailserver.local', 'chris'),
+]
 
-        events = [
-            Event ('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
-            Event ('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
-            Event ('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
-            Event ('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
-            Event ('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
-            Event ('2020-01-23 11:24:35', 'login', 'mailserver.local', 'chris'),
-        ]
-        users = current_users(events)
-        print(users)
+users = current_users(events)
+print(users)
 
-        generate_report(users)
+ generate_report(users)       
